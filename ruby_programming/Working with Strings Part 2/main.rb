7507ea5b2b15017315b411d86a475f8044fa2d5c
falguniislam <islamfalguni12@gmail.com>
2019-11-18 17:10:53 +0100

    working with strings-2

---
+++ ruby_programming/Working with Strings Part 2/main.rb
@@ -0,0 +1,16 @@
+# puts "What was your first name?"
+# first_name = gets.chomp
+# puts "Thank you, you said your first name was # {first_name}"
+
+# puts "Enter a number to multiply by 2"
+# input = gets.chomp
+# puts input * 2
+
+# puts "Enter a number to multiply by 2"
+# input = gets.chomp
+# puts input.to_i * 2
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Whar is your last name?"
+last_name = gets.chomp
