df3bffbf6a40ec89694270c5e948f1203ddcd595
falguniislam <islamfalguni12@gmail.com>
2019-11-18 17:17:03 +0100

    methods added

---
+++ ruby_programming/Methods--ruby/main.rb
@@ -0,0 +1,42 @@
+puts
+puts "Please enter your first number"
+first_number = gets.chomp
+puts "Please enter your second number"
+second_number = gets.chomp
+puts "The first number multiplied by the second numeber is: #{first_number.to_f * second_number.to_f}"
+
+
+# def multiply(first_num, second_num)
+#   first_num.to_f * second_num.to_f
+# end
+
+# def divide(first_num, second_num)
+#   first_num.to_f / second_num.to_f
+# end
+
+# def subtract(first_num, second_num)
+#   first_num.to_f - second_num.to_f
+# end
+
+# def add(first_num, second_num)
+#   first_num.to_f + second_num.to_f
+# end
+
+# def mod(first_num, second_num)
+#   first_num.to_f % second_num.to_f
+# end
+
+# puts "Simple calculator"
+# 20.times { print "-" }
+# puts
+# puts "Please enter your first number"
+# first_number = gets.chomp
+# puts "Please enetr your second number"
+# second_number = gets.chomp
+# puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
+# puts "The first number divided by the second number is: #{first_number.to_f / second_number.to_f}"
+# puts "The first number subtracted from the second number is: #{second_number.to_f - first_number.to_f}"
+# puts "The first number added to the second number is: #{second_number.to_f + first_number.to_f}"
+# puts "The first number mod the second number is: #{first_number.to_f % second_number.to_f}"
+
+
